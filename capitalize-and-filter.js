c3c8d11ab5e86fed9c99b38d25ba40112de1bc4e
Dylan White <dylan.whitej@gmail.com>
2021-03-22 14:23:00 -0700

    capitalizeAndFilter for loop passing

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,5 +1,10 @@
 const capitalizeAndFilter = (arr) => {
-    return arr;
+    const newArr = [];
+    for(let string of arr) {
+        const capString = string.toUpperCase();
+        if(capString.charAt(0) !== 'F') newArr.push(capString)
+    }
+    return newArr;
 }
 
 module.exports = capitalizeAndFilter;
