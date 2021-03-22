0c4f016950f155eb135bb5238b949d9592628ff6
Dylan White <dylan.whitej@gmail.com>
2021-03-22 13:39:53 -0700

    initial commit

---
+++ get-name.test.js
@@ -0,0 +1,14 @@
+const getName = require("./get-name");
+
+describe('get name function', () => {
+
+    it('should return the value of the name property when given an object', () => {
+        const spot = { 
+            name: 'spot', 
+            age: 5, 
+            weight: '20 lbs', 
+        }
+        const result = getName(spot);
+        expect(result).toEqual('spot')
+    } )
+});
