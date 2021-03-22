462f37796552eea72ee44df844c6e4260d3bc553
Dylan White <dylan.whitej@gmail.com>
2021-03-22 13:45:09 -0700

    adds two more tests to bracket notation

--- get-name.test.js
+++ get-name.test.js
@@ -11,4 +11,21 @@ describe('get name function', () => {
         const result = getName(spot);
         expect(result).toEqual('spot')
     })
+    it('should return the value of the name property when given an object', () => {
+        const spot = { 
+            name: 'wheelbarrow', 
+            age: 5, 
+            weight: '20 lbs', 
+        }
+        const result = getName(spot);
+        expect(result).toEqual('wheelbarrow')
+    })
+    it('should return undefined if the name property does not exist in the given object', () => {
+        const spot = {  
+            age: 5, 
+            weight: '20 lbs', 
+        }
+        const result = getName(spot);
+        expect(result).toEqual(undefined)
+    })
 });
