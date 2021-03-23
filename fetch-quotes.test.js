7a88f9975339a1f4de4753badb831ecc7661bffe
Dylan White <dylan.whitej@gmail.com>
2021-03-22 18:02:54 -0700

    mock passing

--- fetch-quotes.test.js
+++ fetch-quotes.test.js
@@ -1,14 +1,28 @@
-const fetchQuote = require("./fetch-quotes");
+const fetchQuote = require('./fetch-quotes');
+jest.mock('node-fetch'); 
+const fetch = require('node-fetch');
 
 describe('test for fetch quotes function', () => {
     it('should return one quote from API in correct format ', async () => {
-
+        fetch.mockImplementation(() => {
+            return Promise.resolve({
+                json: () => {
+                    return [{
+                        character: 'blender',
+                        quote: 'hello',
+                        image: 'image'
+                    }]
+                }
+            })
+        })
         const result = await fetchQuote();
-
-        expect(result).toEqual(expect.objectContaining({
-            name: expect.any(String),
-            text: expect.any(String),
-            image: expect.any(String)
-        }))
+        console.log(result)
+        expect(result).toEqual(
+            expect.objectContaining({
+                name: expect.any(String),
+                text: expect.any(String),
+                image: expect.any(String),
+            })
+        );
     });
 });
